3043bbc13aaffba568d8ad2229036549f3c37eef
nexttoppers <ankitgamer09@gmail.com>
2024-12-12 17:57:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ async def upload(bot: Client, m: Message):
     else:
         MR = raw_text3
    
-    await editable.edit("Now send the Thumb url/nEg » https://graph.org/file/ce1723991756e48c35aa1.jpg \n Or if don't want thumbnail send = no")
+    await editable.edit("Now send the Thumb url/nEg » https://avatars.githubusercontent.com/u/181358091 \n Or if don't want thumbnail send = no")
     input6 = message = await bot.listen(editable.chat.id)
     raw_text6 = input6.text
     await input6.delete(True)
